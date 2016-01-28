0234a53aa27138282671cc69a3c5fe1cd7a5bf63
Brett Rann <brettrann@gmail.com>
2016-01-28 11:06:07 +1100

    Chapter 10: Auto-assign the default state to newly-created tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,10 +1,21 @@
 class Ticket < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
+
   belongs_to :state
   belongs_to :project
   belongs_to :author, class_name: "User"
+
   has_many :attachments, dependent: :destroy
   has_many :comments, dependent: :destroy
+
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
+
+  before_create :assign_default_state
+
+  private
+
+  def assign_default_state
+    self.state ||= State.default
+  end
 end
